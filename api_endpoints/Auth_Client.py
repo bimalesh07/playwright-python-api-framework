7faf987c401bf6f3a779066d7f854e4d412a06b6
Bimalesh <bimaleshk07@gmail.com>
2026-07-15 16:19:05 +0530

    Auth_Client completed

--- api_endpoints/Auth_Client.py
+++ api_endpoints/Auth_Client.py
@@ -1,12 +1,30 @@
-
-
-
 class UserAuthClient:
     def __init__(self, api_context):
         self.api_context = api_context
 
 
-    def check_email_availability(self, email_payload):
+    def get_all_users(self):
+        response = self.api_context.get("/api/v1/users")
+        return response
 
-        return self.api_context.post()
+    def create_user(self, user_payload):
+        response = self.api_context.post("/api/v1/users" data= user_payload)
+        return response
+
+    def get_user_by_id(self, user_id):
+        response = self.api_context.post(f"/api/v1/users/{user_id}")
+        return response
+
+    def update_user(self, user_id, update_payload):
+        response = self.api_context.put(f"/api/v1/users/{user_id}", data=update_payload)
+        return response
+
+    def delete_user(self, user_id):
+        response = self.api_context.delete(f"/api/v1/users/{user_id}")
+        return response
+
+    def check_email_avialabilty(self, email_Payload):
+        response = self.api_context.post("/api/v1/users/is_available", data=email_Payload)
+
+
         
